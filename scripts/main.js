1bc5bde5faab896e9e5a448c2e466bd3f29473a5
David Fucsko <davidzsolt.fucsko@ge.com>
2020-06-02 16:14:41 +0200

    Add Mobile navigation

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+    let navigationItems = document.querySelectorAll('.navigation__item');
+    let closeNavigationButton = document.querySelector('.navigation__checkbox');
+    let closeNavigation = () => {
+        closeNavigationButton.checked = false;
+    };
+    navigationItems.forEach(item => {
+        item.addEventListener('click', () => {
+            closeNavigation();
+        });
+    });
+})();
